bfdf84c16fd45a49a012b7ac2d6f0c413241040a
gutao <542779063@qq.com>
2018-01-08 22:39:32 +0800

    update

--- src/main/java/com/tao/controller/LetouController.java
+++ src/main/java/com/tao/controller/LetouController.java
@@ -4,6 +4,7 @@ import com.tao.service.LetouService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 
@@ -24,7 +25,9 @@ public class LetouController {
     }
 
     @RequestMapping("/index.html")
-    public String index(){
-        return "index";
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("newballs","123456");
+        return modelAndView;
     }
 }
